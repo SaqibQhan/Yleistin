0c5069294c74b4aa4e798f530a1143e5c267887c
saqib <saqib@saqib.(none)>
2013-12-10 19:57:51 +0500

    sign in , sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Yleistin::Application.routes.draw do
-  resource :home
 
-  root 'home#index'
+  devise_for :users, :token_authentication_key => 'authentication_key', :controllers => {:registrations => "devise/registrations", :sessions => "devise/sessions"}
+  devise_scope :user do
+    get "/users/sign_out", to: "devise/sessions#destroy", :via => [:get, :post]
+    get "/users/sign_in", to: "home#index", :via => [:get, :post]
+  end
+
+  resource :home
+ root 'home#index'
+
 end
